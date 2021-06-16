26008af47d1d4dd6334f9e922384e80e8663501f
Toni Schönbuchner <toni.schoenbuchner@csgis.de>
2021-06-16 15:08:05 +0200

    added email templates

--- daard_database/apps.py
+++ daard_database/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class DaardDatabaseConfig(AppConfig):
     name = 'daard_database'
+
+    def ready(self):
+        import daard_database.signals #noqa
---
+++ daard_database/send_notification.py
@@ -0,0 +1,39 @@
+from django.core.mail import send_mail, BadHeaderError
+from django.template.loader import render_to_string
+from smtplib import SMTPException
+import logging
+from django.conf import settings
+
+logger = logging.getLogger(__name__)
+
+
+
+def notify_daard_user(receiver=[], template='', instance='', title=''):
+
+    #msg_html = render_to_string('./email/admin_notice.txt', {'instance': instance.pk})
+    msg_plain = render_to_string(template, {'instance': instance})
+
+    print (str(receiver))
+    print(template)
+    print(instance.__dict__)
+    print("D name is : ")
+    print(instance.disease)
+    print(title)
+
+
+    try:
+        send_mail(
+            'email title',
+            msg_plain,
+            settings.DEFAULT_FROM_EMAIL,
+            receiver
+        )
+    except BadHeaderError:  # If mail's Subject is not properly formatted.
+        logger.debug('Invalid header found.')
+        print('Invalid header found.')
+    except SMTPException as e:  # It will catch other errors related to SMTP.
+        logger.debug('There was an error sending an email.' + e)
+        print('There was an error sending an email.' + e)
+    except:  # It will catch All other possible errors.
+        logger.debug("Mail Sending Failed!")
+        print("Mail Sending Failed!")
