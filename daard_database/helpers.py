6742763b1f8a20c2b514c44bb571a0d90a1b326a
Toni Schönbuchner <toni.schoenbuchner@csgis.de>
2021-10-15 13:25:11 +0200

    update fornmat_bonce for nested bone changes

--- daard_database/helpers.py
+++ daard_database/helpers.py
@@ -25,33 +25,38 @@ def get_svgids(instance):
     svgid_names = ','.join(y.get("svgid","") for x, y in inventory.items())
     return svgid_names
 
+
 def format_bone_relations(instance):
     inventory = getattr(instance, "inventory", {})
     bone_relations = getattr(instance, "bone_relations", {})
     new_dict = {}
 
     for key in inventory.keys():
-        new_dict[key] = inventory[key]
+        item_name = inventory[key]["label"]
+        inventory[key].pop('svgid', None)
+        inventory[key].pop('label', None)
+        inventory[key].pop('id', None)
+        inventory[key].pop('section', None)
+        inventory[key].pop('name', None)
+        new_dict[item_name] = inventory[key]
+        new_dict[item_name]["changes"] = bone_relations[key].get("changes", "")
 
-        for key2, val2 in bone_relations[key].items():
-            if key not in new_dict[key]:
-                new_dict[key][key2] = ''.join(val2) if type(val2) is list else val2
-
-        new_dict[key] = ''.join([f'({key}: {value})' for key, value in new_dict[key].items()])
-
-    c_b_t_bc_rel = ''.join([f'({key}:\n{value})' for key, value in new_dict.items()])
-    c_b_t_bc_rel = c_b_t_bc_rel.replace("<", "less than")
-    c_b_t_bc_rel = c_b_t_bc_rel.replace(">", "more than")
-    c_b_t_bc_rel = c_b_t_bc_rel.replace("\n","").replace("\r","")
-    return  c_b_t_bc_rel
+    c_b_t_bc_rel = ''.join([f'{key} : {value}' for key, value in new_dict.items()])
+    c_b_t_bc_rel = c_b_t_bc_rel.replace("<", "less than ")
+    c_b_t_bc_rel = c_b_t_bc_rel.replace(">", "more than ")
+    return c_b_t_bc_rel
 
 def get_technics(instance):
     bone_relations = getattr(instance, "bone_relations", {})
     technic = []
     for key in bone_relations.keys():
-        t = bone_relations[key].get("technic")
-        if (t is not None):
-            technic.append(t)
+        changes_key = bone_relations[key].get("changes")
+        if (changes_key is not None):
+          for change_item in changes_key:
+              item = change_item.get("technic", None)
+              if item is not None:
+              	technic.append(item)
+
     technic = set(technic)
     technic = ', '.join(technic)
     return technic
